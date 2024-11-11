2a82f0910ca2c9913e927b92c229e9f769d31907
moriyaki <moriyaki999@gmail.com>
2024-11-12 03:35:20 +0900

    プロジェクト ファイルを追加します。

---
+++ MultiLanguageTest/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MultiLanguageTest
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        /// <summary>
+        /// 挨拶をするボタンのテキスト
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static")]
+        public string Greetings
+        {
+            get => ResourceService.GetString("Greetings");
+        }
+
+        /// <summary>
+        /// 日本語にする
+        /// </summary>
+        public RelayCommand ToJapanese { get; set; }
+
+        /// <summary>
+        /// 英語にする
+        /// </summary>
+        public RelayCommand ToEnglish { get; set; }
+
+        /// <summary>
+        /// ロシア語にする
+        /// </summary>
+        public RelayCommand ToRussian { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ToEnglish = new RelayCommand(() => ChangeGreetingsCulture("en"));
+            ToJapanese = new RelayCommand(() => ChangeGreetingsCulture("ja"));
+            ToRussian = new RelayCommand(() => ChangeGreetingsCulture("ru"));
+            ResourceService.ChangeCulture("en");
+        }
+
+        /// <summary>
+        /// 挨拶のためのカルチャーコードを変更します。
+        /// </summary>
+        /// <param name="cultureCode">カルチャーコード</param>
+        private void ChangeGreetingsCulture(string cultureCode)
+        {
+            ResourceService.ChangeCulture(cultureCode);
+            OnPropertyChanged(nameof(Greetings));
+        }
+    }
+}
---
+++ MultiLanguageTest/ResourceService.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace MultiLanguageTest
+{
+    public static class ResourceService
+    {
+        private static ResourceManager resourceManager = new("MultiLanguageTest.Resources.Resource", typeof(ResourceService).Assembly);
+
+        /// <summary>
+        /// 言語を変更します。
+        /// </summary>
+        /// <param name="cultureCode">変更するカルチャーコード</param>
+        public static void ChangeCulture(string cultureCode)
+        {
+            var culture = new CultureInfo(cultureCode);
+            CultureInfo.CurrentUICulture = culture;
+        }
+
+        /// <summary>
+        /// リソースファイルから名前をキーに、カルチャーコードに基づいた文字列を取得します。
+        /// </summary>
+        /// <param name="key">文字列を取得するリソースファイルキー</param>
+        /// <returns>取得したい文字列</returns>
+        public static string GetString(string key)
+        {
+            return resourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? string.Empty;
+        }
+    }
+}
